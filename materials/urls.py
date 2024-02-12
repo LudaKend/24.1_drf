e68fa360f3c755b0d751eb9fcada49997ad11d21
LudaKend <Ludakend@gmail.com>
2024-02-12 15:17:56 +0300

    Задание 3: для модели Lesson реализовала CRUD на Generic-классах

--- materials/urls.py
+++ materials/urls.py
@@ -1,10 +1,18 @@
 from materials.apps import MaterialsConfig
 from rest_framework.routers import DefaultRouter
-from materials.views import CourseViewSet
+from materials.views import CourseViewSet, LessonCreateAPIView, LessonListAPIView, LessonRetriveAPIView, LessonUpdateAPIView, LessonDestroyAPIView
+from django.urls import path
+
 
 app_name = MaterialsConfig.name
 
 router = DefaultRouter()
 router.register(r'materials', CourseViewSet, basename='course')
 
-urlpatterns = [] + router.urls
+urlpatterns = [
+    path('create/', LessonCreateAPIView.as_view(), name='lesson_create'),
+    path('list_view/', LessonListAPIView.as_view(), name='lesson_list'),
+    path('view/<int:pk>/', LessonRetriveAPIView.as_view(), name='lesson_view'),
+    path('update/<int:pk>/', LessonUpdateAPIView.as_view(), name='lesson_update'),
+    path('delete/<int:pk>/', LessonDestroyAPIView.as_view(), name='lesson_delete'),
+              ] + router.urls
